acb2285fdb6cf3858758581dc23cfe5281dc6f79
Pavel Silin <pavelsilinw@gmail.com>
2024-03-20 23:08:21 +0300

    update

--- packages/backend/src/telegram-auth/main.ts
+++ packages/backend/src/telegram-auth/main.ts
@@ -3,16 +3,16 @@ import jwt from "jsonwebtoken";
 import { createHandler } from "../utils";
 import { verifyTelegramWebAppData } from "../utils";
 
-const schema = z.object({
-    initData: z.string(),
-});
-
 const TELEGRAM_BOT_TOKEN = ""; // https://core.telegram.org/bots#creating-a-new-bot
 const JWT_SECRET = "secret";
 const DEFAULT_EXPIRES_IN = 1800;
 
+const schema = z.object({
+    initData: z.string(),
+});
+
 export const handler = createHandler(schema, async data => {
-    const initData = verifyTelegramWebAppData(data.initData, TELEGRAM_BOT_TOKEN);
+    const initData = verifyTelegramWebAppData(data.initData, TELEGRAM_BOT_TOKEN, DEFAULT_EXPIRES_IN);
     if (!initData) {
         return {
             statusCode: 401,
